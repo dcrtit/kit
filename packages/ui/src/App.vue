e780b8ec07d8b9d4a4212589d02026a3dfc69458
Lennoximus <lennoximus@gmail.com>
2022-01-31 15:00:47 +0300

    Chore: Scaffold `ui` package structure

---
+++ packages/ui/src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+  import Breadcrumbs from './Breadcrumbs/Breadcrumbs.vue'
+</script>
+
+<template>
+  <div>
+    <Breadcrumbs/>
+    asdasdasdasd
+  </div>
+</template>
+
+<style/>
---
+++ packages/ui/src/Breadcrumbs/Breadcrumbs.vue
@@ -0,0 +1,15 @@
+<script setup>
+  import {ref} from 'vue-demi'
+
+  const testVModelVar = ref('')
+</script>
+
+<template>
+  <div>
+    Hello, Im breadcrumbs
+    <input type="text" v-model="testVModelVar">
+    {{ testVModelVar }}
+  </div>
+</template>
+
+<style/>
