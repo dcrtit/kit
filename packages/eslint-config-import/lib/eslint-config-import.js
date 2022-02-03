17f8a0ccf2ecbebd2cffd47c536dea9856e720e9
Lennoximus <lennoximus@gmail.com>
2022-02-03 18:58:02 +0300

    Style(eslint-config-import): Update to code style

--- packages/eslint-config-import/lib/eslint-config-import.js
+++ packages/eslint-config-import/lib/eslint-config-import.js
@@ -1,9 +1,9 @@
 module.exports = {
-  'rules': {
+  rules: {
     'import/order': 2,
     'import/first': 2,
     'import/no-mutable-exports': 2,
     'import/no-unresolved': 0,
-    'import/exports-last': 2,
+    'import/exports-last': 2
   }
 }
