a110e27ae57ce8722e5ea318b940af6237b7d642
Alexander <fotomexanik@yandex.ru>
2022-02-08 15:36:55 +0300

    Feat(ui-btn): Setup module structure

---
+++ packages/ui/src/Button/Button.vue
@@ -0,0 +1,19 @@
+<script lang="ts">
+
+</script>
+
+<template>
+  <component :is="componentName"
+             v-bind="options"
+             @click="onClick">
+
+    <slot name="prependIcon"/>
+    <slot/>
+    <slot name="appendIcon"/>
+
+  </component>
+</template>
+
+<style/>
+
+
