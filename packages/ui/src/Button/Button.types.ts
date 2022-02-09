81f8d8586de98a5edc66805179932a4e8f415fa2
Alexander <fotomexanik@yandex.ru>
2022-02-09 19:42:08 +0300

    Fix(ui-btn): Types

--- packages/ui/src/Button/Button.types.ts
+++ packages/ui/src/Button/Button.types.ts
@@ -2,13 +2,13 @@ export type TLinkButtonTag = 'a' | 'router-link' | 'nuxt-link'
 export type TButtonComponent = TLinkButtonTag | 'button'
 export type TButtonType = 'submit' | 'reset' | 'button'
 export type TLinkAttribute = 'href' | 'to'
-export type TPath = String | null
+export type TPath = string | null
 
 
 export interface IButtonOptions {
   disabled?: boolean | null
   type?: TButtonType | null
-  to?: string | null
+  to?: TPath
   event?: 'click' | null
   style?: IStyleOptions
 }
