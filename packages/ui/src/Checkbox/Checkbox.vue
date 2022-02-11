958c15792e9fbd54b8c00562863b83d23fd1c0aa
Alexander <fotomexanik@yandex.ru>
2022-02-11 17:30:34 +0300

    Feat(ui-checkbox): Add props and computed options

--- packages/ui/src/Checkbox/Checkbox.vue
+++ packages/ui/src/Checkbox/Checkbox.vue
@@ -9,13 +9,82 @@ import {
 import {PropType} from "vue";
 
 export default defineComponent ({
-  name: "c-checkbox"
+  name: "c-checkbox",
+  model: {
+    prop: 'modelValue',
+    event: 'change'
+  },
+  props: {
+    //modelValue - может содержать массив [выбранных значений] или просто true/false
+    modelValue: {
+      type: Array as PropType<TCheckboxModel>,
+      default: false
+    },
+    // значение чекбокса
+    value: {
+      type: String as PropType<TCheckboxValue>,
+      default: ''
+    },
+    comparatorField: {
+      type: String,
+      required: false,
+      default: 'value'
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    required: {
+      type: Boolean,
+      default: false
+    },
+    reverse: {
+      type: Boolean,
+      default: false
+    },
+  },
+
+  computed: {
+    isChecked (): boolean {
+      let checked: boolean = false
+
+      if (typeof this.modelValue === 'boolean') {
+        // если булев тип, равно значению modelValue
+        checked = this.modelValue
+      }
+      else if (Array.isArray(this.modelValue)) {
+        // если массив значений
+        if (['string', 'number'].includes(typeof this.value)) {
+          // массив примитивов - проверяем содержит ли он значение "value"
+          checked = this.modelValue.includes(this.value)
+        }
+        else if (typeof this.value === 'object') {
+          // массив объектов проверяем содержит ли он объект "value"
+          checked = Boolean(this.modelValue.find((value: TCheckboxValue): boolean =>
+              (value as TCheckboxValueObject)[this.comparatorField] === (this.value as TCheckboxValueObject)[this.comparatorField]))
+        }
+      }
+
+      return checked
+    },
+
+    checkboxOptions (): ICheckboxOptions {
+      return {
+        disabled: this.disabled,
+        checked: this.isChecked,
+        required: this.required
+      }
+    }
+  },
+
 })
+
 </script>
 
 <template>
   <label >
-    <input type="checkbox">
+    <input type="checkbox"
+           v-bind="checkboxOptions" >
 
     <slot/>
     <slot name="checkboxIcon"></slot>
