5fc8f8d600acc0a083a91a49a0a4cc23fe2a48cf
Lennoximus <lennoximus@gmail.com>
2022-02-15 19:18:02 +0300

    Feat(ui): Setup basic building functionality

---
+++ packages/ui/src/Divider/index.ts
@@ -0,0 +1,3 @@
+import CDivider from './Divider.vue'
+
+export default CDivider
--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -1,10 +1,18 @@
 // eslint-disable-next-line spaced-comment
 /// <reference types="vitest" />
+import * as path from 'path'
 import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: 'ui'
+    },
+    outDir: './lib'
+  },
   plugins: [vue()],
   optimizeDeps: {
     exclude: ['vue-demi']
