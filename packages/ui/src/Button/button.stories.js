7811627310367e75eea34cf079e5e54ea9fb2efc
Alexander <fotomexanik@yandex.ru>
2022-02-16 12:39:09 +0300

    Feat(ui-btn): Add storybook

--- packages/ui/src/Button/button.stories.js
+++ packages/ui/src/Button/button.stories.js
@@ -16,25 +16,97 @@ export default {
   // },
 };
 
-// More on component templates: https://storybook.js.org/docs/vue/writing-stories/introduction#using-args
 const Template = (args) => ({
-  // Components used in your story `template` are defined in the `components` object
   components: { CButton },
-  // The story's `args` need to be mapped into the template through the `setup()` method
   setup() {
-    return { args };
+    const styleBtn = {
+      fontFamily: 'Arial',
+      fontSize: '16px',
+      padding: '12px 24px',
+      borderRadius: '4px',
+      border: '1px solid #5982b9',
+      color: 'black',
+      background: '#c3d8f5',
+      cursor: 'pointer',
+      width: '200px',
+      opacity: args.disabled? '0.6' :'1',
+    }
+
+    return { args, styleBtn, };
   },
-  // And then the `args` are bound to your component with `v-bind="args"`
   template: `
-    <CButton v-bind="args">
-      Кнопка 1
-    </CButton>
+    <div style="width: 500px;">
+      <CButton v-bind="args" :style="styleBtn">
+        Button
+      </CButton>
+    </div>
+    
   `
 });
 
 export const Btn = Template.bind({});
-// More on args: https://storybook.js.org/docs/vue/writing-stories/args
 Btn.args = {
 
 };
+
+export const BtnLink = Template.bind({});
+BtnLink.args = {
+  to: '#'
+};
+
+
+const TemplateSlotIcon = (args) => ({
+  components: { CButton },
+  setup() {
+    const styleBtn = {
+      fontFamily: 'Arial',
+      fontSize: '16px',
+      padding: '12px 24px',
+      borderRadius: '4px',
+      border: '1px solid #5982b9',
+      color: 'black',
+      background: '#c3d8f5',
+      cursor: 'pointer',
+      width: '200px',
+      opacity: args.disabled? '0.6' :'1',
+    }
+    const styleBtnIcon = {
+      margin: '0 8px',
+      background: '#ea5353',
+      height: '40px',
+      width: '40px',
+      display: 'inline-flex',
+      alignItems: 'center',
+      justifyContent: 'center'
+    }
+
+    return { args, styleBtn, styleBtnIcon };
+  },
+  template: `
+    <div style="width: 500px;">
+      <CButton v-bind="args" :style="styleBtn" >
+        <template #prependIcon>
+          <span class="btn-icon" :style="styleBtnIcon">Icon</span>
+        </template>
+        <span style="padding: 0 24px">Button</span>
+        <template #appendIcon>
+          <span class="btn-icon" :style="styleBtnIcon">Icon</span>
+        </template>
+      </CButton>
+    </div>
+    
+  `
+});
+
+export const BtnSlotIcon = TemplateSlotIcon.bind({});
+// More on args: https://storybook.js.org/docs/vue/writing-stories/args
+  BtnSlotIcon.args = {
+
+  };
+
+export const BtnLinkSlotIcon = TemplateSlotIcon.bind({});
+// More on args: https://storybook.js.org/docs/vue/writing-stories/args
+  BtnLinkSlotIcon.args = {
+    to: '#'
+  };
 
